401854210133ebbf2c0bf9b1d4ea6c411906a309
ЦензорНет <179885950+censorliber@users.noreply.github.com>
2024-11-16 17:56:53 +0300

    Update start_domru.bat

--- start_domru.bat
+++ start_domru.bat
@@ -3,7 +3,7 @@ start "zapret: http,https,quic,discord" /min "%~dp0winws.exe" ^
 --wf-tcp=80,443 --wf-udp=443,50000-50020 ^
 --filter-udp=443 --hostlist="%~dp0russia-youtubeQ.txt" --dpi-desync=fake --dpi-desync-repeats=4 --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
 --filter-tcp=443 --hostlist="%~dp0russia-youtubeGV.txt" --dpi-desync=split --dpi-desync-split-pos=1 --dpi-desync-fooling=badseq --dpi-desync-repeats=10 --dpi-desync-autottl=2 --new ^
---filter-tcp=443 --hostlist="%~dp0russia-youtube.txt" --dpi-desync=fake,split2 --dpi-desync-fooling=md5sig --dpi-desync-split-seqovl=2 --dpi-desync-split-pos=2 --dpi-desync-fake-tls="%~dp0tls_clienthello_2.bin" --dpi-desync-ttl=5 --new ^
+--filter-tcp=443 --hostlist="%~dp0russia-youtube.txt" --dpi-desync=split --dpi-desync-split-seqovl=1 --dpi-desync-split-tls=sniext --dpi-desync-fake-tls="%~dp0tls_clienthello_www_google_com.bin" --dpi-desync-ttl=1 --new ^
 --filter-tcp=80 --hostlist="%~dp0russia-blacklist.txt" --dpi-desync=fake,split2 --dpi-desync-fooling=md5sig --dpi-desync-autottl=2 --new ^
 --filter-tcp=443 --hostlist="%~dp0russia-blacklist.txt" --dpi-desync=fake,split2 --dpi-desync-split-seqovl=1 --dpi-desync-split-tls=sniext --dpi-desync-fake-tls="%~dp0tls_clienthello_www_google_com.bin" --dpi-desync-autottl=2 --new ^
 --filter-tcp=443 --hostlist="%~dp0russia-discord.txt" --dpi-desync=syndata --dpi-desync-fake-syndata="%~dp0tls_clienthello_3.bin"  --dpi-desync-ttl=5 --new ^
