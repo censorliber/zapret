d0b20ef3d3a52b78b6a29e5312ccf5ac05744592
ЦензорНет <179885950+censorliber@users.noreply.github.com>
2024-12-19 21:44:24 +0300

    Update start1.cmd

--- start1.cmd
+++ start1.cmd
@@ -5,5 +5,5 @@ start "zapret: http,https,quic" /min "%~dp0/bin/winws.exe" ^
 --filter-tcp=443 --hostlist="%~dp0lists/discord.txt" --dpi-desync=fake,split2 --dpi-desync-split-seqovl=1 --dpi-desync-split-tls=sniext --dpi-desync-fake-tls="%~dp0bin/tls_clienthello_www_google_com.bin" --dpi-desync-ttl=4 --new ^
 --filter-udp=443 --hostlist="%~dp0lists/discord.txt" --dpi-desync=fake,split2 --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic="%~dp0bin/quic_initial_www_google_com.bin" --new ^
 --filter-udp=50000-65535 --dpi-desync=fake,tamper --dpi-desync-any-protocol --dpi-desync-fake-quic="%~dp0bin/quic_initial_www_google_com.bin" --new ^
---filter-tcp=443 --hostlist="%~dp0other.txt" --dpi-desync=fake,split2 --dpi-desync-split-seqovl=1 --dpi-desync-split-tls=sniext --dpi-desync-fake-tls="%~dp0tls_clienthello_2.bin" --dpi-desync-ttl=3 --new ^
---filter-tcp=443 --hostlist="%~dp0faceinsta.txt" --dpi-desync=split2 --dpi-desync-split-seqovl=652 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern="%~dp0bin/tls_clienthello_www_google_com.bin"
+--filter-tcp=443 --hostlist="%~dp0lists/other.txt" --dpi-desync=fake,split2 --dpi-desync-split-seqovl=1 --dpi-desync-split-tls=sniext --dpi-desync-fake-tls="%~dp0tls_clienthello_2.bin" --dpi-desync-ttl=3 --new ^
+--filter-tcp=443 --hostlist="%~dp0lists/faceinsta.txt" --dpi-desync=split2 --dpi-desync-split-seqovl=652 --dpi-desync-split-pos=2 --dpi-desync-split-seqovl-pattern="%~dp0bin/tls_clienthello_www_google_com.bin"
