e7e4b1c32690b18bc385323f7c2bf57e6dbd58ff
Obsidian <123@gmail.com>
2024-12-22 13:06:34 +0300

    Update start_discord4.bat

--- start_discord4.bat
+++ start_discord4.bat
@@ -13,7 +13,7 @@ set BIN=%~dp0bin\
 set LISTS=%~dp0lists\
 
 start "ZAPRET: Ultimate Fix ALT Beeline-Rostelekom" /min "%BIN%winws.exe" --wf-tcp=80,443 --wf-udp=443,50000-65535 ^
---filter-udp=443 --hostlist="%LISTS%\discord.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
+--filter-udp=443 --hostlist="%LISTS%\discord.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_vk_com.bin" --new ^
 --filter-udp=50000-65535 --ipset="%LISTS%\ipset-discord.txt" --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
 --filter-tcp=80 --hostlist="%LISTS%\discord.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --new ^
 --filter-tcp=443 --hostlist="%LISTS%\discord.txt" --dpi-desync=fake,split2 --dpi-desync-ttl=1 --dpi-desync-autottl=5 --dpi-desync-repeats=6 --dpi-desync-fake-tls="%BIN%tls_clienthello_www_google_com.bin" ^
